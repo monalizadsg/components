e514ef40dbdd24cc567ede94ed079559627d5361
monalizadsg <monalizadasig@gmail.com>
2020-08-06 22:11:01 +0800

    Added validation using formik and email-validator

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import ValidatedLoginForm from "./ValidatedLoginForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <ValidatedLoginForm />
     </div>
   );
 }
--- login-form/src/ValidatedLoginForm.js
+++ login-form/src/ValidatedLoginForm.js
@@ -1,51 +1,96 @@
-import React, { Component } from "react";
+import React from "react";
+import "./App.css";
+import { Formik, Form, Field } from "formik";
+import * as EmailValidator from "email-validator";
+// import * as Yup from "yup";
 
-class ValidatedLoginForm extends Component {
-  state = {
-    email: "",
-    password: "",
-  };
+const ValidatedLoginForm = () => {
+  return (
+    <>
+      <h1>Login</h1>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            console.log("Logging in", values);
+            setSubmitting(false);
+          }, 500);
+        }}
+        // validate using email-validator for practice only
+        validate={(values) => {
+          let errors = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (!EmailValidator.validate(values.email)) {
+            errors.email = "Invalid email address";
+          }
 
-  handleInputChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({ [name]: value });
-  };
+          const passwordRegex = /(?=.*[0-9])/;
+          if (!values.password) {
+            errors.password = "Required";
+          } else if (values.password.length < 8) {
+            errors.password = "Password must be atleast 8 characters long";
+          } else if (!passwordRegex.test(values.password)) {
+            errors.password =
+              "Invalid password. Must contain atleast one number";
+          }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(this.state.email, this.state.password);
-  };
+          return errors;
+        }}
+      >
+        {(props) => {
+          const {
+            values,
+            touched,
+            errors,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          } = props;
+          return (
+            <Form>
+              <label htmlFor="email" onSubmit={handleSubmit}>
+                Email
+              </label>
+              <Field
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.email && touched.email && "error"}
+              />
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              placeholder="Enter your email"
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              placeholder="Enter your password"
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <input type="submit" value="Login" />
-        </form>
-      </div>
-    );
-  }
-}
+              <label htmlFor="password">Password</label>
+              <Field
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.password && touched.password && "error"}
+              />
+              {errors.password && touched.password && (
+                <div className="input-feedback">{errors.password}</div>
+              )}
+              <button type="submit" disabled={isSubmitting}>
+                Login
+              </button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </>
+  );
+};
 
 export default ValidatedLoginForm;
